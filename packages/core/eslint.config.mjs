6004e17773e57e6219c5829c259579fb833bb2d7
MO <fengzilong1992@gmail.com>
2025-01-10 22:39:31 +0800

    chore: export createConfig from eslint-config

--- packages/core/eslint.config.mjs
+++ packages/core/eslint.config.mjs
@@ -1,5 +1,5 @@
-import config from '@infra/eslint-config/library.mjs'
+import createConfig, { Target } from '@infra/eslint-config/library.mjs'
 
 export default [
-  ...config,
+  ...createConfig(Target.Browser),
 ]
