f9eaa0e0617ede9e2b8a85e85cac1b52cf1e913a
MO <fengzilong1992@gmail.com>
2026-01-10 23:08:54 +0800

    feat: migrate to tsdown

--- packages/foo/tsdown.config.ts
+++ packages/foo/tsdown.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from 'tsdown'
 
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
   sourcemap: true,
+  clean: true,
+  dts: true,
+  target: false,
 })
